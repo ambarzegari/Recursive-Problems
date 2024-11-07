d4dc264cb51ff3c369428f43dc20e99edb73cc40
am.barzegari <amir4487001@gmail.com>
2024-11-07 20:48:34 +0330

    Write minsweet function

--- Q3-810102409.cpp
+++ Q3-810102409.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <cmath>
 
 using namespace std;
 
@@ -12,6 +13,24 @@ void Input(int &number_of_college, vector<int> &number_of_student_in_college)
         cin >> input;
         number_of_student_in_college.push_back(input);
     }
+}
+
+int Minsweet(vector<int> num_of_student, int college_num)
+{
+    if (college_num == num_of_student.size() - 1)
+    {
+        return 0;
+    }
+    
+    if (college_num == num_of_student.size() - 2)
+    {
+        return abs(num_of_student[college_num]-num_of_student[college_num + 1]);
+    }
+
+    int cost_1 = abs(num_of_student[college_num] - num_of_student[college_num + 1]) + Minsweet(num_of_student, college_num + 1);
+    int cost_2 = abs(num_of_student[college_num] - num_of_student[college_num + 2]) + Minsweet(num_of_student, college_num + 2);
+    
+    return min(cost_1, cost_2);
 }
 
 int main()
